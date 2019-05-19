bbb625e12b807008eeb9d95dfeae4efba9638693
Matt Dawson <mattldawson@gmail.com>
2019-05-19 22:39:52 +0200

    add llvm to build; added JIT objects

--- src/JitDeriv.cpp
+++ src/JitDeriv.cpp
@@ -16,6 +16,16 @@
 
 namespace jit_test {
 
-JitDeriv::JitDeriv(ClassicDeriv classicDeriv) {}
+JitDeriv::JitDeriv(llvm::orc::JITTargetMachineBuilder JTMB, llvm::DataLayout DL,
+                   ClassicDeriv classicDeriv)
+    : ObjectLayer(
+          ES, []() { return llvm::make_unique<llvm::SectionMemoryManager>(); }),
+      CompileLayer(ES, ObjectLayer,
+                   llvm::orc::ConcurrentIRCompiler(std::move(JTMB))),
+      DL(std::move(DL)), Mangle(ES, this->DL),
+      Ctx(llvm::make_unique<llvm::LLVMContext>()) {
+  ES.getMainJITDylib().setGenerator(cantFail(
+      llvm::orc::DynamicLibrarySearchGenerator::GetForCurrentProcess(DL)));
+}
 void JitDeriv::Solve(const double *const state, double *const deriv) {}
 } // namespace jit_test
--- src/JitDeriv.h
+++ src/JitDeriv.h
@@ -15,14 +15,37 @@
 #ifndef COM_JITDERIV_H
 #define COM_JITDERIV_H
 
+#include "llvm/ADT/StringRef.h"
+#include "llvm/ExecutionEngine/JITSymbol.h"
+#include "llvm/ExecutionEngine/Orc/CompileUtils.h"
+#include "llvm/ExecutionEngine/Orc/Core.h"
+#include "llvm/ExecutionEngine/Orc/ExecutionUtils.h"
+#include "llvm/ExecutionEngine/Orc/IRCompileLayer.h"
+#include "llvm/ExecutionEngine/Orc/JITTargetMachineBuilder.h"
+#include "llvm/ExecutionEngine/Orc/RTDyldObjectLinkingLayer.h"
+#include "llvm/ExecutionEngine/SectionMemoryManager.h"
+#include "llvm/IR/DataLayout.h"
+#include "llvm/IR/LLVMContext.h"
+#include <memory>
+
 namespace jit_test {
 
 class ClassicDeriv;
 
 class JitDeriv {
 public:
-  JitDeriv(ClassicDeriv classicDeriv);
+  JitDeriv(llvm::orc::JITTargetMachineBuilder JTMB, llvm::DataLayout DL,
+           ClassicDeriv classicDeriv);
   void Solve(const double *const state, double *const deriv);
+
+private:
+  llvm::orc::ExecutionSession ES;
+  llvm::orc::RTDyldObjectLinkingLayer ObjectLayer;
+  llvm::orc::IRCompileLayer CompileLayer;
+
+  llvm::DataLayout DL;
+  llvm::orc::MangleAndInterner Mangle;
+  llvm::orc::ThreadSafeContext Ctx;
 };
 }
 #endif // COM_JITDERIV_H
