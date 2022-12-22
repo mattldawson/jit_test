a768289111ff432a970646849aef371f3dbba82d
Kyle Shores <kyle.shores44@gmail.com>
2022-12-22 15:23:00 -0600

    unrolling the loop

--- test/derivative/ClassicDeriv.cpp
+++ test/derivative/ClassicDeriv.cpp
@@ -84,22 +84,28 @@ void ClassicDeriv::WritePreprocessedFortran() {
     }
   }
 
+  for (int i_rxn = 0; i_rxn < this->numRxns; ++i_rxn) {
+    module += "    rate = rateConst(" + std::to_string(i_rxn + 1) + ") \n";
+    for (int i_react = 0; i_react < this->numReact[i_rxn]; ++i_react)
+      module += "    rate = rate * state( reactId(" + std::to_string(i_rxn + 1) + "," + std::to_string(i_react + 1) + " )) \n";
+    for (int i_react = 0; i_react < this->numReact[i_rxn]; ++i_react)
+    {
+      module += 
+      "    deriv( reactId(" + std::to_string(i_rxn + 1) + "," + std::to_string(i_react + 1) + ")) = "
+          "deriv( reactId(" + std::to_string(i_rxn + 1) + "," + std::to_string(i_react + 1) + ")) - rate \n";
+    }
+    for (int i_prod = 0; i_prod < this->numProd[i_rxn]; ++i_prod)
+    {
+      module += 
+      "    deriv( prodId(" + std::to_string(i_rxn + 1) + "," + std::to_string(i_prod + 1) + ")) = "
+          "deriv( prodId(" + std::to_string(i_rxn + 1) + "," + std::to_string(i_prod + 1) + ")) + rate \n";
+    }
+  }
+
   module += 
-  "\n"
-  "    do rxnIdx = 1, " + std::to_string(this->numRxns) + " \n"
-  "      rate = rateConst(rxnIdx) \n"
-  "      do reactIdx = 1, numReact(rxnIdx) \n"
-  "        rate = rate * state( reactId(rxnIdx, reactIdx ) ) \n"
-  "      end do \n"
-  "      do reactIdx = 1, numReact(rxnIdx) \n"
-  "        deriv( reactId(rxnIdx,reactIdx) ) = deriv( reactId(rxnIdx,reactIdx) ) - rate \n"
-  "      end do \n"
-  "      do prodIdx = 1, numProd(rxnIdx) \n"
-  "        deriv( prodId(rxnIdx,prodIdx) ) = deriv( prodId(rxnIdx,prodIdx) ) + rate \n"
-  "      end do \n"
-  "    end do \n"
-  "  end subroutine preprocessed_solve\n"
-  "end module mod\n";
+    "\n"
+    "  end subroutine preprocessed_solve\n"
+    "end module mod\n";
   
   std::cout << module << std::endl;
 }
