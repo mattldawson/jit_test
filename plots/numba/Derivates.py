2477a49660445f2bba83db6cf6daf9e3f3ab6d33
Kyle Shores <kyle.shores44@gmail.com>
2023-01-11 11:42:38 -0600

    added better way to time numba jit compile time

--- plots/numba/Derivates.py
+++ plots/numba/Derivates.py
@@ -2,12 +2,15 @@ import numpy as np
 import numba as nb
 from numba import int64, float64
 
+import inspect
+import sys
+
 spec = [
   ('n_reactions', int64),
   ('n_species', int64),
   ('rate_constants', float64[:]),
   ('number_of_reactants', int64[:]),
-  ('number_of_prodcuts', int64[:]),
+  ('number_of_products', int64[:]),
   ('reactant_id', int64[:, :]),
   ('product_id', int64[:, :]),
 ]
@@ -19,7 +22,7 @@ class ClassicDeriv:
     self.n_species = n_species
     self.rate_constants = np.zeros((self.n_reactions), dtype=np.float64)
     self.number_of_reactants = np.zeros((self.n_reactions), dtype=np.int64)
-    self.number_of_prodcuts = np.zeros((self.n_reactions), dtype=np.int64)
+    self.number_of_products = np.zeros((self.n_reactions), dtype=np.int64)
     self.reactant_id = np.zeros((self.n_reactions, 3), dtype=np.int64)
     self.product_id = np.zeros((self.n_reactions, 10), dtype=np.int64)
 
@@ -27,7 +30,7 @@ class ClassicDeriv:
     np.random.seed(1856)
     self.rate_constants = (np.random.randint(0, np.iinfo(np.int64).max, self.n_reactions) % 10000 + 1) / 100.0
     self.number_of_reactants = np.random.randint(0, np.iinfo(np.int64).max, self.n_reactions) % 2 + 2
-    self.number_of_prodcuts = np.random.randint(0, np.iinfo(np.int64).max, self.n_reactions) % 10 + 1
+    self.number_of_products = np.random.randint(0, np.iinfo(np.int64).max, self.n_reactions) % 10 + 1
 
     self.reactant_id[:] = 0
     self.product_id[:] = 0
@@ -35,7 +38,7 @@ class ClassicDeriv:
     for rxn_idx in range(self.n_reactions):
       for reactant_idx in range(self.number_of_reactants[rxn_idx]):
         self.reactant_id[rxn_idx, reactant_idx] = np.random.randint(0, np.iinfo(np.int64).max) % self.n_species
-      for product_idx in range(self.number_of_prodcuts[rxn_idx]):
+      for product_idx in range(self.number_of_products[rxn_idx]):
         self.product_id[rxn_idx, product_idx] = np.random.randint(0, np.iinfo(np.int64).max) % self.n_species
 
   def solve(self, state, deriv):
@@ -46,7 +49,7 @@ class ClassicDeriv:
         rate *= state[self.reactant_id[rxn_idx, reactant_idx]]
       for reactant_idx in range(self.number_of_reactants[rxn_idx]):
         deriv[self.reactant_id[rxn_idx, reactant_idx]] -= rate
-      for product_idx in range(self.number_of_prodcuts[rxn_idx]):
+      for product_idx in range(self.number_of_products[rxn_idx]):
         deriv[self.product_id[rxn_idx, product_idx]] += rate
 
   def apply(self, parent):
@@ -54,7 +57,7 @@ class ClassicDeriv:
     self.n_species = parent.n_species
     self.rate_constants = np.array(parent.rate_constants)
     self.number_of_reactants = np.array(parent.number_of_reactants)
-    self.number_of_prodcuts = np.array(parent.number_of_prodcuts)
+    self.number_of_products = np.array(parent.number_of_products)
     self.reactant_id = np.array(parent.reactant_id)
     self.product_id = np.array(parent.product_id)
 
@@ -68,7 +71,7 @@ class JITDeriv:
     self.n_species = n_species
     self.rate_constants = np.zeros((self.n_reactions), dtype=np.float64)
     self.number_of_reactants = np.zeros((self.n_reactions), dtype=np.int64)
-    self.number_of_prodcuts = np.zeros((self.n_reactions), dtype=np.int64)
+    self.number_of_products = np.zeros((self.n_reactions), dtype=np.int64)
     self.reactant_id = np.zeros((self.n_reactions, 3), dtype=np.int64)
     self.product_id = np.zeros((self.n_reactions, 10), dtype=np.int64)
   
@@ -76,7 +79,7 @@ class JITDeriv:
     np.random.seed(1856)
     self.rate_constants = (np.random.randint(0, np.iinfo(np.int64).max, self.n_reactions) % 10000 + 1) / 100.0
     self.number_of_reactants = np.random.randint(0, np.iinfo(np.int64).max, self.n_reactions) % 2 + 2
-    self.number_of_prodcuts = np.random.randint(0, np.iinfo(np.int64).max, self.n_reactions) % 10 + 1
+    self.number_of_products = np.random.randint(0, np.iinfo(np.int64).max, self.n_reactions) % 10 + 1
 
     self.reactant_id[:] = 0
     self.product_id[:] = 0
@@ -84,7 +87,7 @@ class JITDeriv:
     for rxn_idx in range(self.n_reactions):
       for reactant_idx in range(self.number_of_reactants[rxn_idx]):
         self.reactant_id[rxn_idx, reactant_idx] = np.random.randint(0, np.iinfo(np.int64).max) % self.n_species
-      for product_idx in range(self.number_of_prodcuts[rxn_idx]):
+      for product_idx in range(self.number_of_products[rxn_idx]):
         self.product_id[rxn_idx, product_idx] = np.random.randint(0, np.iinfo(np.int64).max) % self.n_species
 
   def solve(self, state, deriv):
@@ -95,5 +98,36 @@ class JITDeriv:
         rate *= state[self.reactant_id[rxn_idx, reactant_idx]]
       for reactant_idx in range(self.number_of_reactants[rxn_idx]):
         deriv[self.reactant_id[rxn_idx, reactant_idx]] -= rate
-      for product_idx in range(self.number_of_prodcuts[rxn_idx]):
+      for product_idx in range(self.number_of_products[rxn_idx]):
         deriv[self.product_id[rxn_idx, product_idx]] += rate
+
+@nb.jit(nopython=True) 
+  # signature_or_function=nb.void(
+  #   nb.float64[:],  # state
+  #   nb.float64[:],  # deriv
+  #   nb.int64,  # n_reactions
+  #   nb.int64[:],  # number_of_reactants
+  #   nb.float64[:],  # rate_constants
+  #   nb.int64[:],  # number_of_products
+  #   nb.int64[:, :],  # reactant_id
+  #   nb.int64[:, :]), # product_id
+  # locals=dict(rate=nb.float64))
+def solve(
+    state, 
+    deriv, 
+    n_reactions, 
+    number_of_reactants, 
+    rate_constants, 
+    number_of_products,
+    reactant_id, 
+    product_id
+  ):
+  deriv[:] = 0
+  for rxn_idx in range(n_reactions):
+    rate = rate_constants[rxn_idx]
+    for reactant_idx in range(number_of_reactants[rxn_idx]):
+      rate *= state[reactant_id[rxn_idx, reactant_idx]]
+    for reactant_idx in range(number_of_reactants[rxn_idx]):
+      deriv[reactant_id[rxn_idx, reactant_idx]] -= rate
+    for product_idx in range(number_of_products[rxn_idx]):
+      deriv[product_id[rxn_idx, product_idx]] += rate
--- plots/numba/numba_jit_test.py
+++ plots/numba/numba_jit_test.py
@@ -1,15 +1,54 @@
-from Derivates import ClassicDeriv, JITDeriv
-from numba import config
+from Derivates import ClassicDeriv, JITDeriv, solve
+import numba as nb
 
 import numpy as np
 import pandas as pd
 import time
+import os
+
+# doesn't seem to work, but from here
+# https://stackoverflow.com/questions/44131691/how-to-clear-cache-or-force-recompilation-in-numba
+def kill_files(folder):
+    for the_file in os.listdir(folder):
+        file_path = os.path.join(folder, the_file)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+        except Exception as e:
+            print("failed on filepath: %s" % file_path)
+
+
+def kill_numba_cache():
+
+    root_folder = os.path.realpath(__file__ + "/../../")
+
+    for root, dirnames, filenames in os.walk(root_folder):
+        for dirname in dirnames:
+            if dirname == "__pycache__":
+                try:
+                    kill_files(root + "/" + dirname)
+                except Exception as e:
+                    print("failed on %s", root)
 
 def timeit(thing, state, deriv, iter=10_000):
-  start = time.time_ns()
-  for i in range(iter):
-    thing.solve(state, deriv)
-  end = time.time_ns()
+  if isinstance(thing, JITDeriv):
+    start = time.time_ns()
+    for i in range(iter):
+      solve(
+        state, deriv,
+        n_reactions = thing.n_reactions, 
+        number_of_reactants = thing.number_of_reactants, 
+        rate_constants = thing.rate_constants, 
+        number_of_products = thing.number_of_products,
+        reactant_id = thing.reactant_id, 
+        product_id = thing.product_id
+      )
+    end = time.time_ns()
+  else:
+    start = time.time_ns()
+    for i in range(iter):
+      thing.solve(state, deriv)
+    end = time.time_ns()
   return end - start, deriv.copy()
 
 def table_it():
@@ -19,8 +58,8 @@ def table_it():
   jit_times = []
   jit_compile_times = []
 
-  for nreactions in [10, 100, 500, 1000, 2500, 5000]:
-    for nspecies in [10, 100, 500, 1000, 1500, 2000]:
+  for nreactions in reversed([10, 100, 500, 1000, 2500, 5000]):
+    for nspecies in reversed([10, 100, 500, 1000, 1500, 2000]):
       classic_deriv = ClassicDeriv()
       jit_deriv = JITDeriv(n_reactions = nreactions, n_species = nspecies)
 
@@ -30,17 +69,26 @@ def table_it():
       state = (np.random.randint(0, np.iinfo(np.int64).max, classic_deriv.n_species) % 100) / 100.0
       deriv = np.zeros(classic_deriv.n_species)
 
-      classic_time, classic_results = timeit(classic_deriv, state, deriv)
-      prime_jit_time, _ = timeit(jit_deriv, state, deriv, iter=1)
+      classic_time = 0
+      # classic_time, classic_results = timeit(classic_deriv, state, deriv)
+
+      with nb.core.event.install_listener('numba:compile', nb.core.event.TimingListener()) as res:
+        _ = timeit(jit_deriv, state, deriv, iter=1)
+        if res.done:
+          # res._duration is in fractional seconds
+          # multiple by 1e6 to get to microseconds
+          jit_compile_times.append(res._duration * 1e6)
+        else:
+          jit_compile_times.append(0)
+
       jit_time, jit_results = timeit(jit_deriv, state, deriv)
 
       reactions.append(nreactions)
       species.append(nspecies)
       classic_times.append(classic_time / 1000) # microseconds
-      jit_compile_times.append(prime_jit_time / 1000) # microseconds
       jit_times.append(jit_time / 1000) # microseconds
 
-      assert(np.all(classic_results == jit_results))
+      # assert(np.all(classic_results == jit_results))
   df = pd.DataFrame(
     {
       'reactions': reactions,
