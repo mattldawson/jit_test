f23d694511622a7ef6cd0ce6a21d9d90ddd2eef3
Kyle Shores <kyle.shores44@gmail.com>
2023-03-28 15:44:40 -0500

    again

--- test/derivative/deriv_openacc.cpp
+++ test/derivative/deriv_openacc.cpp
@@ -42,17 +42,17 @@ std::chrono::duration<long, std::nano> deriv_openacc(ClassicDeriv cd,
 #pragma omp target teams thread_limit(VECTOR_LENGTH)
 #pragma omp distribute parallel for simd
 #endif
-  for (int i_cell = 0; i_cell < NUM_CELL; ++i_cell) {
-    for (int i_spec = 0; i_spec < NUM_SPEC; ++i_spec)
-      deriv[i_cell * NUM_SPEC + i_spec] = 0.0;
-    for (int i_rxn = 0; i_rxn < NUM_RXN; ++i_rxn) {
-      rate = rateConst[i_cell * NUM_RXN + i_rxn];
+  for (int i_cell = 0; i_cell < cd.numCell; ++i_cell) {
+    for (int i_spec = 0; i_spec < cd.numSpec; ++i_spec)
+      deriv[i_cell * cd.numSpec + i_spec] = 0.0;
+    for (int i_rxn = 0; i_rxn < cd.numRxns; ++i_rxn) {
+      rate = rateConst[i_cell * cd.numRxns + i_rxn];
       for (int i_react = 0; i_react < cd.numReact[i_rxn]; ++i_react)
-        rate *= state[i_cell * NUM_SPEC + cd.reactId[i_rxn][i_react]];
+        rate *= state[i_cell * cd.numSpec + cd.reactId[i_rxn][i_react]];
       for (int i_react = 0; i_react < cd.numReact[i_rxn]; ++i_react)
-        deriv[i_cell * NUM_SPEC + cd.reactId[i_rxn][i_react]] -= rate;
+        deriv[i_cell * cd.numSpec + cd.reactId[i_rxn][i_react]] -= rate;
       for (int i_prod = 0; i_prod < cd.numProd[i_rxn]; ++i_prod)
-        deriv[i_cell * NUM_SPEC + cd.prodId[i_rxn][i_prod]] += rate;
+        deriv[i_cell * cd.numSpec + cd.prodId[i_rxn][i_prod]] += rate;
     }
   }
   auto stop = std::chrono::high_resolution_clock::now();
