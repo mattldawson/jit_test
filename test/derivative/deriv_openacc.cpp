4b654912d7081bec8b46d440fc62c4344024f038
Kyle Shores <kyle.shores44@gmail.com>
2023-03-28 13:19:51 -0500

    trying to resolve forward declaration

--- test/derivative/deriv_openacc.cpp
+++ test/derivative/deriv_openacc.cpp
@@ -1,61 +1,69 @@
+#include "ClassicDeriv.h"
+#include <chrono>
+#include <cmath>
 #include <cstdio>
 #include <iostream>
-#include <cmath>
 #include <openacc.h>
-#include <chrono>
-#include "ClassicDeriv.h"
 
 // define vector length for OpenACC
-#define VECTOR_LENGTH   128 
+#define VECTOR_LENGTH 128
 
-std::chrono::duration<long, std::nano> deriv_openacc(ClassicDeriv cd, double* rateConst, double* state, double* deriv)
-{
-    // Perform the calculation on GPU/device
-    auto start = std::chrono::high_resolution_clock::now();
+namespace jit_test {
 
-    #ifdef _OPENACC
-    #pragma acc enter data copyin(rateConst[0:cd.numRxns * cd.numCell], \
-                                  state[0:cd.numCell * cd.numCell], \
-                                  cd.numReact[0:cd.numRxns],cd.numProd[0:cd.numRxns], \
-                                  cd.reactId[0:cd.numRxns][0:MAX_REACT], \
-                                  cd.prodId[0:cd.numRxns][0:MAX_PROD]) \
-                           create(deriv[0:cd.numSpec * cd.numCell])
-    #elif defined(_OPENMP)
-    #pragma omp target enter data map(to:rateConst[0:cd.numRxns * cd.numCell], \
-                                  state[0:cd.numCell * cd.numCell], \
-                                  cd.numReact[0:cd.numRxns],cd.numProd[0:cd.numRxns], \
-                                  cd.reactId[0:cd.numRxns][0:MAX_REACT], \
-                                  cd.prodId[0:cd.numRxns][0:MAX_PROD] \
-                                  map(alloc:deriv[0:cd.numSpec * cd.numCell])
-    #endif
+std::chrono::duration<long, std::nano> deriv_openacc(ClassicDeriv cd,
+                                                     double *rateConst,
+                                                     double *state,
+                                                     double *deriv) {
+  // Perform the calculation on GPU/device
+  auto start = std::chrono::high_resolution_clock::now();
 
-    #ifdef _OPENACC
-    #pragma acc parallel default(present)
-    #pragma loop gang vector vector_length(VECTOR_LENGTH)
-    #elif defined(_OPENMP)
-    #pragma omp target teams thread_limit(VECTOR_LENGTH)
-    #pragma omp distribute parallel for simd 
-    #endif
-    for (int i_cell = 0; i_cell < NUM_CELL; ++i_cell){
-        for (int i_spec = 0; i_spec < NUM_SPEC; ++i_spec)
-            deriv[i_cell*NUM_SPEC+i_spec] = 0.0;
-        for (int i_rxn = 0; i_rxn < NUM_RXN; ++i_rxn) {
-            rate = rateConst[i_cell*NUM_RXN+i_rxn];
-            for (int i_react = 0; i_react < hnumReact[i_rxn]; ++i_react)
-                rate *= state[i_cell*NUM_SPEC+hreactId[i_rxn][i_react]];
-            for (int i_react = 0; i_react < hnumReact[i_rxn]; ++i_react)
-                deriv[i_cell*NUM_SPEC+hreactId[i_rxn][i_react]] -= rate;
-            for (int i_prod = 0; i_prod < hnumProd[i_rxn]; ++i_prod)
-                deriv[i_cell*NUM_SPEC+hprodId[i_rxn][i_prod]] += rate;
-        }
+#ifdef _OPENACC
+#pragma acc enter data copyin(                                                 \
+    rateConst [0:cd.numRxns * cd.numCell], state [0:cd.numCell * cd.numCell],  \
+    cd.numReact [0:cd.numRxns], cd.numProd [0:cd.numRxns],                     \
+    cd.reactId [0:cd.numRxns] [0:MAX_REACT],                                   \
+    cd.prodId [0:cd.numRxns] [0:MAX_PROD])                                     \
+    create(deriv [0:cd.numSpec * cd.numCell])
+#elif defined(_OPENMP)
+#pragma omp target enter data map(                                             \
+    to                                                                         \
+    : rateConst [0:cd.numRxns * cd.numCell],                                   \
+      state [0:cd.numCell * cd.numCell], cd.numReact [0:cd.numRxns],           \
+      cd.numProd [0:cd.numRxns], cd.reactId [0:cd.numRxns] [0:MAX_REACT],      \
+      cd.prodId                                                                \
+      [0:cd.numRxns] [0:MAX_PROD] map(alloc                                    \
+                                      : deriv [0:cd.numSpec * cd.numCell])
+#endif
+
+#ifdef _OPENACC
+#pragma acc parallel default(present)
+#pragma loop gang vector vector_length(VECTOR_LENGTH)
+#elif defined(_OPENMP)
+#pragma omp target teams thread_limit(VECTOR_LENGTH)
+#pragma omp distribute parallel for simd
+#endif
+  for (int i_cell = 0; i_cell < NUM_CELL; ++i_cell) {
+    for (int i_spec = 0; i_spec < NUM_SPEC; ++i_spec)
+      deriv[i_cell * NUM_SPEC + i_spec] = 0.0;
+    for (int i_rxn = 0; i_rxn < NUM_RXN; ++i_rxn) {
+      rate = rateConst[i_cell * NUM_RXN + i_rxn];
+      for (int i_react = 0; i_react < hnumReact[i_rxn]; ++i_react)
+        rate *= state[i_cell * NUM_SPEC + hreactId[i_rxn][i_react]];
+      for (int i_react = 0; i_react < hnumReact[i_rxn]; ++i_react)
+        deriv[i_cell * NUM_SPEC + hreactId[i_rxn][i_react]] -= rate;
+      for (int i_prod = 0; i_prod < hnumProd[i_rxn]; ++i_prod)
+        deriv[i_cell * NUM_SPEC + hprodId[i_rxn][i_prod]] += rate;
     }
+  }
 
-    #ifdef _OPENACC
-    #pragma acc exit data copyout(deriv[0:cd.numSpec * cd.numCell])
-    #elif defined(_OPENMP)
-    #pragma omp target exit data map(from:deriv[0:cd.numSpec * cd.numCell])
-    #endif
-    auto stop = std::chrono::high_resolution_clock::now();
+#ifdef _OPENACC
+#pragma acc exit data copyout(deriv [0:cd.numSpec * cd.numCell])
+#elif defined(_OPENMP)
+#pragma omp target exit data map(from : deriv [0:cd.numSpec * cd.numCell])
+#endif
+  auto stop = std::chrono::high_resolution_clock::now();
 
-    return std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
+  return std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
 }
+
+} // namespace jit_test
--- test/derivative/deriv_openacc.h
+++ test/derivative/deriv_openacc.h
@@ -2,6 +2,10 @@
 
 #include <chrono>
 
+namespace jit_test {
+
 class ClassicDeriv;
 
-std::chrono::duration<long, std::nano> deriv_openacc(ClassicDeriv cd, double* rateConst, double* state, double* deriv);
+std::chrono::duration<long, std::nano>
+deriv_openacc(ClassicDeriv cd, double *rateConst, double *state, double *deriv);
+} // namespace jit_test
